6729ab3f210c81cd4248bbe99744d83e5c6a643f
“Ana” <amartinezaguilar6@gmail.com>
2018-03-11 20:32:20 +0100

    Installed axios and used it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class App extends Component {
+
+
   render() {
+
+    const apiUrl = "https://pokeapi.co/api/v2/pokemon/2/";
+
+    axios.get(apiUrl)
+    .then( function (response) {
+      console.log(response);
+      console.log("Este es el nombre: ", response.data.forms[0].name);
+      console.log("Esta es la imagen: ", response.data.sprites.front_default);
+      console.log("Este es el tipo: ", response.data.types[1].type.name);
+      console.log("Este es el número: ", response.data.id);
+    })
+
+
+    // .catch(function (error) {
+    //        pokemonName.innerHTML = "(An error has occurred.)";
+    //        pokemonImage.src = "";
+    //    });
+    //
+
     return (
       <p>Hola</p>
     );
