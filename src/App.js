0f5522e9fdb77e6b467981ec92c7a86cde4025df
“Ana” <amartinezaguilar6@gmail.com>
2018-03-12 10:49:46 +0100

    Obtained data of 25 pokemon

--- src/App.js
+++ src/App.js
@@ -7,23 +7,27 @@ class App extends Component {
 
   render() {
 
-    const apiUrl = "https://pokeapi.co/api/v2/pokemon/2/";
+    function sendRequest() {
+      axios.get(apiUrl)
+      .then( function (response) {
+        console.log(response);
+        console.log("Este es el nombre: ", response.data.forms[0].name);
+        console.log("Esta es la imagen: ", response.data.sprites.front_default);
+        console.log("Este es el tipo: ", response.data.types[0].type.name);
+        console.log("Este es el número: ", response.data.id);
+      })
+    }
 
-    axios.get(apiUrl)
-    .then( function (response) {
-      console.log(response);
-      console.log("Este es el nombre: ", response.data.forms[0].name);
-      console.log("Esta es la imagen: ", response.data.sprites.front_default);
-      console.log("Este es el tipo: ", response.data.types[1].type.name);
-      console.log("Este es el número: ", response.data.id);
-    })
+    let i;
+    let numberPokemon = 1;
+    let apiUrl;
 
 
-    // .catch(function (error) {
-    //        pokemonName.innerHTML = "(An error has occurred.)";
-    //        pokemonImage.src = "";
-    //    });
-    //
+    for (i = 0; i <= 25; i++) {
+      apiUrl = "https://pokeapi.co/api/v2/pokemon/" + numberPokemon + "/";
+      numberPokemon = numberPokemon + 1;
+      sendRequest();
+    }
 
     return (
       <p>Hola</p>
